bbaca9cc3506e99bd1907f5701ac9ad411622210
uiuxkimabe <uiuxkimabe@gmail.com>
2023-07-01 22:23:32 +0700

    add filter category

--- src/js/index.js
+++ src/js/index.js
@@ -17,3 +17,109 @@ inputField.addEventListener("input", () => {
     element.style.display = text.includes(inputValue) ? "block" : "none";
   });
 });
+
+// filter
+const filterNav = document.querySelectorAll(".navigasi ul li");
+const all = document.querySelectorAll(".card");
+const snack = document.querySelectorAll(".snack");
+const pasta = document.querySelectorAll(".pasta");
+const indo = document.querySelectorAll(".indo");
+const chin = document.querySelectorAll(".chinese");
+const bev = document.querySelectorAll(".bev");
+console.info(filterNav);
+
+filterNav[0].addEventListener("click", () => {
+  all.forEach((element) => {
+    element.style.transform = "block";
+  });
+});
+
+filterNav[1].addEventListener("click", () => {
+  snack.forEach((element) => {
+    element.style.transform = "block";
+  });
+  pasta.forEach((element) => {
+    element.style.transform = "none";
+  });
+  indo.forEach((element) => {
+    element.style.transform = "none";
+  });
+  chin.forEach((element) => {
+    element.style.transform = "none";
+  });
+  bev.forEach((element) => {
+    element.style.transform = "none";
+  });
+});
+
+filterNav[2].addEventListener("click", () => {
+  snack.forEach((element) => {
+    element.style.transform = "none";
+  });
+  pasta.forEach((element) => {
+    element.style.transform = "block";
+  });
+  indo.forEach((element) => {
+    element.style.transform = "none";
+  });
+  chin.forEach((element) => {
+    element.style.transform = "none";
+  });
+  bev.forEach((element) => {
+    element.style.transform = "none";
+  });
+});
+
+filterNav[3].addEventListener("click", () => {
+  snack.forEach((element) => {
+    element.style.transform = "none";
+  });
+  pasta.forEach((element) => {
+    element.style.transform = "none";
+  });
+  indo.forEach((element) => {
+    element.style.transform = "block";
+  });
+  chin.forEach((element) => {
+    element.style.transform = "none";
+  });
+  bev.forEach((element) => {
+    element.style.transform = "none";
+  });
+});
+
+filterNav[4].addEventListener("click", () => {
+  snack.forEach((element) => {
+    element.style.transform = "none";
+  });
+  pasta.forEach((element) => {
+    element.style.transform = "none";
+  });
+  indo.forEach((element) => {
+    element.style.transform = "none";
+  });
+  chin.forEach((element) => {
+    element.style.transform = "block";
+  });
+  bev.forEach((element) => {
+    element.style.transform = "none";
+  });
+});
+
+filterNav[5].addEventListener("click", () => {
+  snack.forEach((element) => {
+    element.style.transform = "none";
+  });
+  pasta.forEach((element) => {
+    element.style.transform = "none";
+  });
+  indo.forEach((element) => {
+    element.style.transform = "none";
+  });
+  chin.forEach((element) => {
+    element.style.transform = "none";
+  });
+  bev.forEach((element) => {
+    element.style.transform = "block";
+  });
+});
